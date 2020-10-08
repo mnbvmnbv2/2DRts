a7ab66ec1657de011dfb8f67e684809a1eb6ffaa
mnbvmnbv2 <32966421+mnbvmnbv2@users.noreply.github.com>
2020-10-08 11:11:11 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -2,23 +2,23 @@ let staticUI = [];
 let movingUI = [];
 
 class UIElement {
-	constructor(pic, x, y, width, height, isStaticUI, clickFunc) {
-		this.width = width * pixelSize;
-		this.height = height * pixelSize;
-		let pict = new Image(this.width, this.height);
-		pict.src = 'pictures/' + pic;
-		this.pic = pict;
-		this.x = x;
-		this.y = y;
-		this.clickFunc = clickFunc;
+  constructor(pic, x, y, width, height, isStaticUI, clickFunc) {
+    this.width = width * pixelSize;
+    this.height = height * pixelSize;
+    let pict = new Image(this.width, this.height);
+    pict.src = "pictures/" + pic;
+    this.pic = pict;
+    this.x = x;
+    this.y = y;
+    this.clickFunc = clickFunc;
 
-		if (isStaticUI) {
-			staticUI.push(this);
-		} else {
-			movingUI.push(this);
-		}
-	}
-	/*
+    if (isStaticUI) {
+      staticUI.push(this);
+    } else {
+      movingUI.push(this);
+    }
+  }
+  /*
 	clickFunc() {
 		switch (this.clickFuncType) {
 			case 'buildWood':
@@ -31,7 +31,7 @@ class UIElement {
 			case 'buildBase':
 				new Building('Base', 'buildings/SmallRoom4x.png', this.x - 168, this.y - 66, 100, 50, 4);
 				movingUI.splice(movingUI.indexOf(this), 1);
-				break;
+				break; 
 			default:
 				break;
 		}
@@ -40,7 +40,15 @@ class UIElement {
 }
 
 function buildBase(ui) {
-	let a = new Building('Base', 'buildings/SmallRoom4x.png', ui.x - 168, ui.y - 66, 100, 50, 4);
-	movingUI.splice(movingUI.indexOf(ui), 1);
-	return a;
+  let a = new Building(
+    "Base",
+    "buildings/SmallRoom4x.png",
+    Math.floor((ui.x - 168) / pixelSize) * pixelSize,
+    Math.floor((ui.y - 66) / pixelSize) * pixelSize,
+    100,
+    50,
+    4
+  );
+  movingUI.splice(movingUI.indexOf(ui), 1);
+  return a;
 }
